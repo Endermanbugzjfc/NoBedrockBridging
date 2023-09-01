f7fc15299124a422492ef56270f6f35a09a0d67b
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2023-09-01 18:22:53 +0800

    Format: PHP CS Fixer.

--- src/plugin.php
+++ src/plugin.php
@@ -5,44 +5,54 @@ declare(strict_types=1);
 namespace Endermanbugzjfc\NoBedrockBridging;
 
 use cosmicpe\npcdialogue\NpcDialogueManager;
-use pocketmine\event\Listener;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use function class_exists;
+use function str_starts_with;
+use function yaml_parse;
 
 class Main extends PluginBase implements Listener {
     protected function onEnable() : void {
         foreach ([
             NpcDialogueManager::class,
-            Await::class,
-        ] as $virion) if (!class_exists($virion)) {
-            $this->getLogger()->error("Please re-download the plugin PHAR from https://poggit.pmmp.io/p/NoBedrockBridging");
-            $this->getServer()->getPluginManager()->disablePlugin($this);
-            return;
+        ] as $virion) {
+            if (!class_exists($virion)) {
+                $this->getLogger()->error("Please re-download the plugin PHAR from https://poggit.pmmp.io/p/NoBedrockBridging");
+                $this->getServer()->getPluginManager()->disablePlugin($this);
+                return;
+            }
         }
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        if(!NpcDialogueManager::isRegistered()){
+        if (!NpcDialogueManager::isRegistered()) {
             NpcDialogueManager::register($this);
         }
-        if (!DialogueEntity::register()) $this->getLogger()->warning("Without the Customies plugin, player will only be able to see messages in plain text.");
+        if (!DialogueEntity::register()) {
+            $this->getLogger()->warning("Without the Customies plugin, player will only be able to see messages in plain text.");
+        }
 
         foreach ($this->getResources() as $path => $file) {
-            if (!str_starts_with($path, "translations/")) continue;
+            if (!str_starts_with($path, "translations/")) {
+                continue;
+            }
             $fileObject = $file->openFile();
             $raw = $fileObject->fread($file->getSize());
             unset($fileObject);
 
             $data = yaml_parse($raw);
             $entries = $data["dialogue"];
-            foreach ($data["codes"] as $code) $this->messages[$code] = new DialogueMessages(
-                title: TextFormat::colorize($entries["title"]),
-                body: TextFormat::colorize($entries["body"]),
-            );
+            foreach ($data["codes"] as $code) {
+                $this->messages[$code] = new DialogueMessages(
+                    title: TextFormat::colorize($entries["title"]),
+                    body: TextFormat::colorize($entries["body"]),
+                );
+            }
         }
     }
 
@@ -53,16 +63,24 @@ class Main extends PluginBase implements Listener {
 
     public function onBlockPlace(BlockPlaceEvent $event) : void {
         $player = $event->getPlayer();
-        if ($player->isFlying()) return;
-        if ($player->hasPermission("nobedrockbridging.bypass")) return;
+        if ($player->isFlying()) {
+            return;
+        }
+        if ($player->hasPermission("nobedrockbridging.bypass")) {
+            return;
+        }
 
         $against = $event->getBlockAgainst()->getPosition();
         $down = $player->getPosition()->floor()->down();
-        if (!$against->equals($down)) return;
+        if (!$against->equals($down)) {
+            return;
+        }
         $forwardDown = $down->getSide($player->getHorizontalFacing());
         foreach ($event->getTransaction()->getBlocks() as [$x, $y, $z, $block]) {
             $newPos = new Vector3($x, $y, $z);
-            if (!$newPos->equals($forwardDown)) continue;
+            if (!$newPos->equals($forwardDown)) {
+                continue;
+            }
 
             $event->cancel();
             $teleportPos = $against->up()->add(.5, 0, .5);
@@ -86,7 +104,9 @@ class Main extends PluginBase implements Listener {
 
     public function onPlayerMove(PlayerMoveEvent $event) : void {
         $player = $event->getPlayer();
-        if (!isset($this->movementLocks[$player->getId()])) return;
+        if (!isset($this->movementLocks[$player->getId()])) {
+            return;
+        }
 
         $event->cancel();
     }
