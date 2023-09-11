7ec87bd3673cd1c35351754c2d6143a628b70a43
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2023-09-11 23:02:14 +0800

    Logics: dialogue entity should not crash server, just despawn on damage (attack()) instead.

--- poggit_stage/src/Endermanbugzjfc/NoBedrockBridging/DialogueMessages.php
+++ poggit_stage/src/Endermanbugzjfc/NoBedrockBridging/DialogueMessages.php
@@ -8,14 +8,11 @@ use Closure;
 use Endermanbugzjfc\NoBedrockBridging\libs\_4a8dc1fd09822184\cosmicpe\npcdialogue\NpcDialogueBuilder;
 use Endermanbugzjfc\NoBedrockBridging\libs\_4a8dc1fd09822184\cosmicpe\npcdialogue\NpcDialogueManager;
 use customiesdevs\customies\entity\CustomiesEntityFactory;
-use Exception;
 use pocketmine\entity\EntitySizeInfo;
 use pocketmine\entity\Living;
-use pocketmine\entity\Location;
-use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\player\Player;
 use pocketmine\plugin\Plugin;
-use RuntimeException;
 
 class DialogueMessages {
     public function __construct(
